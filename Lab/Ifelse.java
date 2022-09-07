6ad1ce9928007a47ba12cf9422a44cfc082bd44c
Ashutara <99647384+Ashutara@users.noreply.github.com>
2022-09-07 16:39:20 +0530

    Add files via upload

---
+++ Lab/Ifelse.java
@@ -0,0 +1,17 @@
+//demo if..else
+class  Ifelse
+{
+public static void main(String[] args) 
+	{
+	int n;
+	n=-10;
+	if(n>0)
+		{
+		System.out.println("positive");
+		}
+		else
+		{
+		System.out.println("negative");
+		}
+	}
+}
